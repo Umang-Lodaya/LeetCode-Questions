dead09c94ffb94c96a81c2621d66c23b50a7e019
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-01-14 13:01:34 +0530

    Time: 52 ms (73.46%), Space: 15.6 MB (16.63%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+import re
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        return s == s[::-1]
