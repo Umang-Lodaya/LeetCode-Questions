8181e6b07b5eecd1b0929c7c29b8c9c24a680f1a
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-01-14 12:51:48 +0530

    Time: 44 ms (46.76%), Space: 13.8 MB (64.39%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        C = []
+        for num in range(1, numRows+1):
+            ls = []
+            for i in range(num):
+                if(i == 0 or i==num-1):
+                    ls.append(1)
+                else:
+                    e = C[num-2][i-1] + C[num-2][i]
+                    ls.append(e)
+            
+            # print(ls)
+            C.append(ls)
+        # print(C)
+        return C
