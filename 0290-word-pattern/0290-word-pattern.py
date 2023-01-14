9704d1cd19c14c9743d1d6e942be26efd1b35e6a
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-01-14 17:28:05 +0530

    Time: 27 ms (89.38%), Space: 13.8 MB (98.12%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        
+        if len(pattern) != len(s.split()):
+            return False
+        
+        if len(set(list(pattern))) != len(set(s.split())):
+            return False
+        
+        mappings = {}
+        for p,s in zip(pattern, s.split()):
+            
+            if p not in mappings:
+                mappings[p] = [s]
+            else:
+                mappings[p].append(s)
+        
+        for key in mappings:
+            if len(set(mappings[key])) != 1:
+                return False
+            
+        return True
