b189ca2acb9c623552d8d437e5d93c2f4f11a3de
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-01-17 16:20:58 +0530

    Time: 3034 ms (36.86%), Space: 180.1 MB (70.97%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+
+        tree = defaultdict(list)
+        for s,e in edges:
+            tree[s].append(e)
+            tree[e].append(s)
+        
+        res = [0] * n
+        
+        def dfs(node, par):
+            nonlocal res
+            count = Counter()
+            for child in tree[node]:
+                if child != par:
+                    count += dfs(child, node)
+            
+            count[labels[node]] += 1
+            res[node] = count[labels[node]]
+            
+            return count
+        
+        dfs(0,-1)
+        return res
