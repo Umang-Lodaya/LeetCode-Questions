5e500a000f0e7a4f0ebdb422661f9625c910de5e
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-08-11 12:19:11 +0530

    Time: 394 ms (49.38%), Space: 37.6 MB (32.31%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,19 @@
 class Solution:
-    def ways(self, amount, coins, i, dp):
-        if i == 0:
-            return 1 if amount % coins[0] == 0 else 0
-        
-        if dp[i][amount] != -1:
-            return dp[i][amount]
-        
-        notTake = self.ways(amount, coins, i-1, dp)
-        take = 0
-        if amount >= coins[i]:
-            take = self.ways(amount - coins[i], coins, i, dp)
-        
-        dp[i][amount] =  notTake + take
-        return dp[i][amount]
-    
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[-1 for j in range(amount+1)] for i in range(len(coins))]
-        return self.ways(amount, coins, len(coins) - 1, dp)
+        dp = [[0 for j in range(amount+1)] for i in range(len(coins))]
+        
+        for j in range(amount + 1):
+            if j % coins[0] == 0:
+                dp[0][j] = 1
+        
+        for i in range(1, len(coins)):
+            for j in range(amount + 1):
+        
+                notTake = dp[i-1][j]
+                take = 0
+                if amount >= coins[i]:
+                    take = dp[i][j - coins[i]]
+
+                dp[i][j] =  notTake + take
+        
+        return dp[len(coins) - 1][amount]
