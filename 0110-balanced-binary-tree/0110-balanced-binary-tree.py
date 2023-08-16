b86a7ecbdd51c9006ecd4837664e1808d9eef928
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-08-16 18:51:32 +0530

    Time: 54 ms (83.79%), Space: 21 MB (76.46%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -3,7 +3,9 @@ class Solution(object):
         return (self.Height(root) >= 0)
     
     def Height(self, root):
-        if root is None:  return 0
+        if not root:  
+            return 0
+        
         leftHeight, rightHeight = self.Height(root.left), self.Height(root.right)
         
         if leftHeight < 0 or rightHeight < 0 or abs(leftHeight - rightHeight) > 1:  
