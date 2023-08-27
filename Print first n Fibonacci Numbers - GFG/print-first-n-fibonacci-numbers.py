bec617e54b761d2e196115347f7838300d2ce4ac
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-08-27 11:19:18 +0530

    Added solution - LeetHub

---
+++ Print first n Fibonacci Numbers - GFG/print-first-n-fibonacci-numbers.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    #Function to return list containing first n fibonacci numbers.
+    def printFibb(self, n):
+        if n == 1:
+            return [1]
+            
+        a = 0
+        b = 1
+        ans = [1]
+        
+        for i in range(n - 1):
+            c = a + b
+            ans.append(c)
+            a = b
+            b = c
+        
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__': 
+    t=int(input())
+    for tcs in range(t):
+        
+        n=int(input())
+        res = Solution().printFibb(n)
+        for i in range (len(res)):
+            print (res[i], end = " ") 
+        print()
+# } Driver Code Ends
