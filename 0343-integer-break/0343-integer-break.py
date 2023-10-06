8e01025cf2eb25a0330b68075efebc6fc0800155
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-10-06 08:29:09 +0530

    Time: 43 ms (39.16%), Space: 16.1 MB (85.57%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def integerBreak(self, n):
         dp = [None, 1]
+        
         for m in range (2, n + 1):
             j = m - 1
             i = 1
@@ -9,5 +10,7 @@ class Solution(object):
                 max_product = max(max_product, max(i, dp[i]) * max(j, dp[j]))
                 j -= 1
                 i += 1
+                
             dp.append(max_product)
+            
         return dp[n]
