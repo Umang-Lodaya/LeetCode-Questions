d504eac9b399a1b6e0264e4453a656c047811214
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-10-20 11:19:45 +0530

    Time: 45 ms (81.11%), Space: 17.6 MB (33.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,17 @@
 import re
 
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        s = re.sub(r'[^a-zA-Z0-9]', '', s)
-        return s == s[::-1]
+        s = re.sub('[^0-9a-zA-Z]+', '', s)
+        
+        if s == "":
+            return True
+        
+        i = 0
+        while i < len(s) // 2:
+            if s[i] != s[len(s) - 1 - i]:
+                return False
+            i += 1
+            
+        return True
